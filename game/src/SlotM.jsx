b7b9927e92b0c97a40ef0f5a58cbaa0aaea23b1a
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-23 21:33:13 +0500

    Update SlotM.jsx

--- game/src/SlotM.jsx
+++ game/src/SlotM.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 const SlotM=(props) =>{
-    let x=props.x;
-    let y=props.y;
-    let z=props.z;
-    if(x==y && x==z){
-    return (<div className="slot_inner">
-      <h1>{"  "+x+"  "+y+"  "+z+"  "}</h1>
-      <h1>This is Matching.</h1>
-      <hr/>
-      </div>);}
-    else{
+    let {x,y,z}=props;
+    let Matching="";
+    (x==y && x==z)?Matching="Congrats, It is Matching!!":Matching="Sorry, It isn't Matching!!";
       return (<div className="slot_inner">
     <h1>{"  "+x+"  "+y+"  "+z+"  "}</h1
-      ><h1>This is not Matching.</h1
-      ><hr></hr>
-      </div>);}
+      ><h1>{Matching}</h1><hr/>
+      </div>);
+
   }
   export default SlotM;
