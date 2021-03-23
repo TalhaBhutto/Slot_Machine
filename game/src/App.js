bda13a12576212d0bd172d18235c3ba11f494d60
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-23 21:18:29 +0500

    slots

--- game/src/App.js
+++ game/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+//import SlotM from "./SlotM";
 
+const SlotM=(props) =>{
+  let x=props.x;
+  let y=props.y;
+  let z=props.z;
+  if(props.x==props.y && props.x==z){
+  return (<div className="slot_inner">
+    <h1>{"  "+x+"  "+y+"  "+z+"  "}</h1>
+    <h1>This is Matching.</h1>
+    <hr/>
+    </div>)}
+  else{
+  <div className="slot_inner">
+    return (<h1>{"  "+x+"  "+y+"  "+z+"  "}</h1
+    ><h1>This is not Matching.</h1
+    ><hr></hr>
+    </div>)}
+};
 function App() {
   return (
-    <div className="App">
+    <>
       <h1 className="Heading_Style">🎰 Welcome to <span style={{fontWeight:"bold"}}> Slot Machine Game</span> 🎰</h1>
-    </div>
+      <div>
+      <SlotM x="😄" y="😄" z="😄" />
+      <SlotM x="😄" y="❤️" z="🍌" />
+      <SlotM x="🍌" y="❤️" z="🍌"/>
+      </div>
+    </>
   );
 }
 
